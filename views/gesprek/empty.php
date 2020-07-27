6316036587e0d6edef2f40120c0c86096ca419e0
Max Bisschop <max@iMac.local>
2020-07-27 13:03:33 +0200

    P3

--- views/gesprek/empty.php
+++ views/gesprek/empty.php
@@ -2,8 +2,6 @@
 
 use yii\helpers\Html;
 
-use nex\datepicker\DatePicker;
-
 ?>
 
 <div class="Login Succes">
@@ -20,16 +18,3 @@ use nex\datepicker\DatePicker;
 
 <br><br>
 <?= Html::a('Nieuwe Gespreksaanvraag', ['/gesprek/create'], ['class' => 'btn btn-success']) ?>
-
-<?= DatePicker::widget([
-    'name' => 'datepickerTest',
-    'value' => '09/13/2015',
-    'clientOptions' => [
-        'format' => 'L',
-    ],
-    'dropdownItems' => [
-        ['label' => 'Yesterday', 'url' => '#', 'value' => \Yii::$app->formatter->asDate('-1 day')],
-        ['label' => 'Tomorrow', 'url' => '#', 'value' => \Yii::$app->formatter->asDate('+1 day')],
-        ['label' => 'Some value', 'url' => '#', 'value' => 'Special value'],
-    ],
-]);?>
